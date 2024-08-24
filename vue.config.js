cba148dc781cc74d610c4a2a3deee78d3dafd966
Berk Uralcan <berkuralcan@gmail.com>
2024-08-25 02:47:56 +0300

    build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: "https://berkuralcan.github.io/altevren-image-optimizer/",
   transpileDependencies: true
 })
