879889b9b0bd2ab7fd7f60ab9e9fadd9a8bf2d3e
Berk Uralcan <berkuralcan@gmail.com>
2024-08-25 06:01:08 +0300

    redeploy for ssl

--- src/functions/aws.js
+++ src/functions/aws.js
@@ -2,10 +2,12 @@ import axios from 'axios';
 
 const API_ENDPOINT = process.env.VUE_APP_API_ENDPOINT; // EC2 public IP or domain
 
-export const uploadFile = async (file) => {
+export const uploadFile = async (file, name, desiredWidth) => {
   console.log(API_ENDPOINT);
   const formData = new FormData();
   formData.append('file', file);
+  formData.append('name', name);
+  formData.append('desiredWidth', desiredWidth);
 
   try {
     const response = await axios.post(`${API_ENDPOINT}/upload`, formData, {
